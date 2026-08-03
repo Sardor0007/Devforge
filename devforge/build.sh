ea88dd183afa118c4ffb4d117116bc38e530229b
Sardor <sardorikromov3138@gmail.com>
2026-08-03 23:29:24 +0500

    Superuser avtomatik yaratish qoshildi

--- devforge/build.sh
+++ devforge/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-# Render build script
 set -o errexit
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
 python manage.py makemigrations accounts projects assets marketplace workspace notifications messaging
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+"
