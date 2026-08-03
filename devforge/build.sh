59913aac62200aa101d665a4cf94b63ff73e079e
Sardor <sardorikromov3138@gmail.com>
2026-08-03 23:59:03 +0500

    build.sh: makemigrations olib tashlandi

--- devforge/build.sh
+++ devforge/build.sh
@@ -2,7 +2,6 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations accounts projects assets marketplace workspace notifications messaging
 python manage.py migrate
 
 python manage.py shell -c "
